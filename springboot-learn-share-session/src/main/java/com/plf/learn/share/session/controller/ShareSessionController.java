982b8597d8d3a459a0b53c468b53a01cd9e3df39
Panlf <liangfeng_pan@163.com>
2019-12-05 14:11:23 +0800

    git edit

--- springboot-learn-share-session/src/main/java/com/plf/learn/share/session/controller/ShareSessionController.java
+++ springboot-learn-share-session/src/main/java/com/plf/learn/share/session/controller/ShareSessionController.java
@@ -1,8 +1,6 @@
 package com.plf.learn.share.session.controller;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,21 +13,23 @@ import javax.servlet.http.HttpSession;
  * @date 2019/12/4
  */
 @RestController
-@Slf4j
 public class ShareSessionController {
 
+
+    @Value("${server.port}")
+    public int port;
+
     @GetMapping("/create/{name}")
-    public ResponseEntity<Void> createSession(HttpServletRequest request, @PathVariable String name){
+    public String createSession(HttpServletRequest request, @PathVariable String name){
         HttpSession session=request.getSession();
         session.setAttribute("name",name);
-        log.info("当前项目的session.id:{},name为{}已成功存入session",session.getId(),name);
-        return ResponseEntity.status(HttpStatus.OK).build();
+        return "当前端口"+port+"项目的session.id:"+session.getId()+",name为"+name+"已成功存入session";
+
     }
 
     @GetMapping("/get")
-    public ResponseEntity<Void> getSession(HttpServletRequest request){
+    public String getSession(HttpServletRequest request){
         HttpSession session=request.getSession();
-        log.info("当前项目的session.id:{},获取name为{}",session.getId(),session.getAttribute("name"));
-        return ResponseEntity.status(HttpStatus.OK).build();
+        return  "当前端口"+port+"项目的session.id:"+session.getId()+",name为"+session.getAttribute("name");
     }
 }
